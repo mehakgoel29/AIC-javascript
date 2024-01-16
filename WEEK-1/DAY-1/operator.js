324c1d06d6770e354814939a143067ce2289f249
mehakgoel29 <mhkgoel29@gmail.com>
2024-01-16 22:54:10 +0530

    day1

--- WEEK-1/DAY-1/operator.js
+++ WEEK-1/DAY-1/operator.js
@@ -1,3 +1,4 @@
+// arithmetic operator
 let a= 3;
 let b = 2;
 let c = a+b;
@@ -6,3 +7,9 @@ let d = a-b;
 console.log(d)
 console.log(a%b)
 console.log(a/b)
+
+
+//assignment operator 
+x=5
+x=+5
+console.log(x)
