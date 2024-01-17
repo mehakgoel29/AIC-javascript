051b5fd448620de3ab7701e0d058171e0d2f4b04
mehakgoel29 <mhkgoel29@gmail.com>
2024-01-17 21:18:12 +0530

    day1

--- WEEK-1/DAY-1/variables.js
+++ WEEK-1/DAY-1/variables.js
@@ -10,3 +10,15 @@ y=5
 z=x+y
 console.log(z);
 
+// using var
+var a=3
+var b=4
+console.log(a+b)
+
+// using const 
+const  d=3
+const  e=4
+console.log(e*d)
+const fruits="apple"
+const fruit="banana"
+console.log(fruits+fruit)
