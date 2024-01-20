822063b1645eb3e7ef62cdf3b540bc401110570a
mehakgoel29 <mhkgoel29@gmail.com>
2024-01-20 21:16:44 +0530

    day 5

---
+++ WEEK-1/DAY-5/anonymousFunction.js
@@ -0,0 +1,14 @@
+// anonymous function- function without name 
+// without parameter 
+var m = function(){
+    console.log("hello world");
+}
+m();
+
+// with parameter
+let parameter;
+var n=function(parameter){
+    console.log("hello",parameter)
+}
+
+n("everyone");
---
+++ WEEK-1/DAY-5/expressionFunction.js
@@ -0,0 +1,12 @@
+function add(x,y){
+    return x+y;
+}
+
+console.log("addition of two numbers: "+ add(6,6))
+
+var add= function(x,y){
+    z=x+y;
+    console.log(x,"+",y ,"=",z)
+}
+
+add(3,4);
