4e69ee016b5790fb3e240dfcfbfd9c72f8abd017
mehakgoel29 <mhkgoel29@gmail.com>
2024-01-24 21:17:44 +0530

    practice

---
+++ PRACTICE-WEEK-2 .js/DAY1.js
@@ -0,0 +1,53 @@
+//Store two integers in two variables x and y. Print the sum of the two
+let x = 5
+let y = 10
+console.log(x + y)
+
+//Store two integers in two variables x and y. Print the product of the two
+let a = 3
+let b = 8
+console.log(a * b)
+
+//Calculate the average of 5 subjects
+let sub1 = 34
+let sub2 = 56
+let sub3 = 78
+let sub4 = 88
+let sub5 = 53
+let avg = (sub1 + sub2 + sub3 + sub4 + sub5) / 5
+console.log(avg)
+
+//Calculate Area of a Circle, square, rectangle, triangle
+let radius = 5
+let length = 50
+let breadth = 60
+let side = 5
+let base = 2
+let height = 4
+let area_circle = console.log(3.14 * radius * radius)
+let area_rectangle = console.log(length * breadth)
+let area_square = console.log(side ** 2)
+let area_triangle = console.log(0.5 * base * height)
+
+//Calculate the simple interest
+let principle = 7
+let rate = 2
+let time = 2
+let simple_interest = console.log(principle * rate * time)
+
+//Calculate the circumference of circle
+let r = 2  // radius
+let circumference = console.log(2 * 3.14 * r)
+
+//  Write a program to perform all the arithmetic operations [except increment and
+// decrement operators] of JavaScript of any two numbers stored in the variables num1
+// and num2. Also, print the results to the console
+let num1 = 10
+let num2 = 5
+let sum = console.log(num1 + num2)
+let diff = console.log(num1 - num2)
+let mul = console.log(num1 * num2)
+let div = console.log(num1 / num2)
+let exp = console.log(num1 ** num2)
+let rem = console.log(num1 % num2)
+
