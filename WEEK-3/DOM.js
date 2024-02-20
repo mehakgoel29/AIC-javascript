98b7c000420f101afad2dc65e8a5d194b2c0ab94
mehakgoel29 <mhkgoel29@gmail.com>
2024-02-20 22:15:12 +0530

    DOM

--- WEEK-3/DOM.js
+++ WEEK-3/DOM.js
@@ -1,3 +1,23 @@
-console.log(window);
-console.log(window.document);
-console.dir(window.document);
+// console.log(window);
+// console.log(window.document);
+// console.dir(window.document);
+// console.log(document.body.childNodes[1]);
+// document.body.style.backgroundColor='pink'
+let heading=document.getElementsByClassName("head");
+console.dir(heading);
+let para=document.getElementById('para');
+console.dir(para);
+console.log(document.getElementsByTagName('p'));
+
+//queryselector
+let firstel=document.querySelector(".head")
+console.log(firstel);
+let allel=document.querySelectorAll("p")
+console.log(allel);
+
+//properties
+console.log(firstel.tagName);
+let  div= document.querySelector('.d')
+console.log(div.innerText);
+console.dir(div.innerHTML);
+console.log(document.querySelector('div').children)
